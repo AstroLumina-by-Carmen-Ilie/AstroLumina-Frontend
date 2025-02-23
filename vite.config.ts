0d419f29c93f9fd7e81d9adc10404932de3ef107
Pirvu Daniel Catalin <daniel.catalin.pirvu@gmail.com>
2025-02-23 10:51:59 +0200

    Simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,27 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',
+  // base: '/',
   plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-  },
-  build: {
-    target: ['es2022', 'chrome92', 'edge92', 'firefox91', 'safari15'],
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor': ['react', 'react-dom', 'react-router-dom'],
-          'i18n': ['i18next', 'react-i18next'],
-          'utils': ['axios', 'moment']
-        }
-      }
-    },
-    minify: 'esbuild',
-    sourcemap: true
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react']
-  }
+  // server: {
+  //   port: 5173,
+  //   host: true,
+  // },
+  // build: {
+  //   target: ['es2022', 'chrome92', 'edge92', 'firefox91', 'safari15'],
+  //   rollupOptions: {
+  //     output: {
+  //       manualChunks: {
+  //         'vendor': ['react', 'react-dom', 'react-router-dom'],
+  //         'i18n': ['i18next', 'react-i18next'],
+  //         'utils': ['axios', 'moment']
+  //       }
+  //     }
+  //   },
+  //   minify: 'esbuild',
+  //   sourcemap: true
+  // },
+  // optimizeDeps: {
+  //   exclude: ['lucide-react']
+  // }
 });
